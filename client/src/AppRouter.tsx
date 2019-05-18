16a7a2158eb6377cb126e567663d6108fab0da7f
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-05-18 18:44:20 +1000

    Add Create a project button, update project registration url

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -3,13 +3,12 @@ import { linkgen, Paths } from 'common/helpers/pathing';
 import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Logout from 'pages/Logout';
-import ProjectRegister from 'pages/ProjectRegistration';
 import ProjectPage from 'pages/ProjectPage';
+import ProjectRegister from 'pages/ProjectRegistration';
 import Projects from 'pages/Projects';
 import Signup from 'pages/Signup';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -18,7 +17,7 @@ const AppRouter = () => (
         <Route exact path={linkgen(Paths.home)} component={Home} />
         <Route path={linkgen(Paths.login)} component={Login} />
         <Route path={linkgen(Paths.signup)} component={Signup} />
-        <Route exact path="/projectregistration" component={ProjectRegister} />
+        <Route exact path="/project-registration" component={ProjectRegister} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/projects/:id" component={ProjectPage} />
         <Route path={linkgen(Paths.logout)} component={Logout} />
@@ -28,4 +27,3 @@ const AppRouter = () => (
 );
 
 export default AppRouter;
-
