7ba9e7f9909ef317b327b69212d67e29489f43f1
jvdsouza <jvincentdsouza@gmail.com>
2019-05-18 17:42:04 +1000

    Add form

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -3,11 +3,13 @@ import { linkgen, Paths } from 'common/helpers/pathing';
 import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Logout from 'pages/Logout';
+import ProjectRegister from 'pages/ProjectRegistration';
 import ProjectPage from 'pages/ProjectPage';
 import Projects from 'pages/Projects';
 import Signup from 'pages/Signup';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -16,6 +18,7 @@ const AppRouter = () => (
         <Route exact path={linkgen(Paths.home)} component={Home} />
         <Route path={linkgen(Paths.login)} component={Login} />
         <Route path={linkgen(Paths.signup)} component={Signup} />
+        <Route exact path="/projectregistration" component={ProjectRegister} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/projects/:id" component={ProjectPage} />
         <Route path={linkgen(Paths.logout)} component={Logout} />
@@ -24,4 +27,4 @@ const AppRouter = () => (
   </BrowserRouter>
 );
 
-export default AppRouter;
+export default AppRouter
--- client/src/pages/ProjectRegistration/index.tsx
+++ client/src/pages/ProjectRegistration/index.tsx
@@ -1,20 +1,82 @@
 import Main from 'common/components/Main';
-import React from 'react';
+import React, {Component} from 'react';
 
-const ProjectRegister = () => {
+class ProjectRegister extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+        projectName: '',
+        microNeed: '',
+        itemAmount: '',
+        //choice: 
+        kidAmount: '',
+        importance: '',
+        meaning: ''
+    }
+  }
+  //TODO on change
+  onChange = (event: any)  => this.setState({ [event.target.name]: event.target.value });
+  //TODO on submit
+  onSubmit = () => {
+    return this.state
+  }
+
+  render() {
   return (
     <Main>
       <h1>Project registration</h1>
+      <input 
+        type='text' 
+        name='projectName' 
+        placeholder='project name' 
+        onChange={this.onChange}
+      />
+      {/*TODO multiple microneeds*/}
       <h3>What is your MicroNeed</h3>
-      <h3>Number of items</h3>
+      <span>
+        <input 
+            type='text' 
+            name='microNeed' 
+            placeholder='Microneed' 
+            onChange={this.onChange}
+          />
+        <input type='text' name='itemAmount' placeholder='Number Of Items' />
+      </span>
+
       <h3>Are you looking for:</h3>
-      <h4>Items to be sent directly to you</h4>
-      <h4>Funding to source the item locally</h4>
+      {/* TODO add unordered list for the radio buttons
+    https://magnusbenoni.com/radio-buttons-react/*/ }
+      <div>
+        <input type='radio' value="direct"/> Items to be sent directly
+        <input type='radio' value="local"/> Funding to source the item locally
+      </div>
+
       <h3>How many kids will this help</h3>
+      <input 
+        type='text' 
+        name="kidAmount" 
+        placeholder="Amount of kids helped" 
+        onChange={this.onChange}
+      />
+
+      {/*TODO word limitation*/}
       <h3>Why is this so important(150 words)</h3>
-      <h3>If we can meet htis need, what will it mean for your kids?(150 words)</h3>
+      <textarea 
+        name="importance" 
+        placeholder="Importance of the project" 
+        onChange={this.onChange}
+      />
+
+      <h3>If we can meet this need, what will it mean for your kids?(150 words)</h3>
+      <textarea  
+        name='meaning' 
+        placeholder='Meaning to the kids' 
+        onChange={this.onChange}
+      />
+
+      <button onClick={this.onSubmit} />
     </Main>
-  );
+  )}
 };
 
 export default ProjectRegister;
